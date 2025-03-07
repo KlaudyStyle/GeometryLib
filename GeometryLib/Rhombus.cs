12949bb4d22b9a259852a3601620acc4726bb02b
KlaudyStyle <mr.vlad.volkov2000@mail.ru>
2025-03-07 21:27:55 +0300

    07.03.2025

---
+++ GeometryLib/Rhombus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLib
+{
+    public class Rhombus : Parallelogram
+    {
+        public Rhombus(Point a, Point b, Point c, Point d) : base(a, b, c, d)
+        {
+        }
+
+        public override double CalculateArea()
+        {
+            double[] diagonals = CalculateDiagonals();
+            return (diagonals[0] * diagonals[1]) / 2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
